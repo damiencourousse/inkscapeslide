af510793da29df23baeeed54f93a4b64d1ace503
Damien Couroussé <damien.courousse@cea.fr>
2011-09-16 17:37:03 +0200

    export area : drawing borders only

--- build/lib.linux-x86_64-2.6/inkscapeslide/__init__.py
+++ build/lib.linux-x86_64-2.6/inkscapeslide/__init__.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python
 # -=- encoding: utf-8 -=-
+# Author: Alexandre Bourget
+# Copyright (c) 2008: Alexandre Bourget
+# LICENSE: GPLv3
 
-"""
-Author: Alexandre Bourget
-Copyright (c) 2008: Alexandre Bourget
-LICENSE: GPLv3
+# How to use this script
+# ------------------------
+# Create a "content" labeled layer and put a text box (no flowRect), with each
+# line looking like:
+#
+#   background, layer1
+#   background, layer2
+#   background, layer2, layer3
+#   +layer4
+#   background, layer2 * 0.5, layer3 * 0.5, layer5
+#
 
-inkscapeslide is a simple tool to generate slides from inkscape files.
-
-See --help for more.
-"""
 
 import lxml.etree
 import sys
@@ -21,45 +27,18 @@ from optparse import OptionParser
 
 def main():
     import warnings
-    # HIDE DEPRECATION WARNINGS ONLY IN RELEASES. SHOW THEM IN DEV. TRUNKS
+    # HIDE DEPRECATION WARINGS ONLY IN RELEASES. SHOW THEM IN DEV. TRUNKS
     warnings.filterwarnings('ignore', category=DeprecationWarning)
 
-    # optparse setup
-    usage = """
-%prog [options] svgfilename
-
-inkscapeslide is a simple tool to generate slides from inkscape files.
-
-Create a 'content'-labeled layer and put a text box (no flowRect), with
-each line looking like:
-
-   background, layer1
-   background, layer2
-   background, layer2, layer3
-   background, layer2 * 0.5, layer
-   +layer4 * 0.5
-
-Each name being the label of a layer. Lines starting with a '+' will add
-the named layer to the layers of the preceding line, creating
-incremental display (note there must be no whitespace before '+')
-
-The opacity of a layer can be changed by adding '*[0., 1]' after the
-layer name. The opacity must be between 0 and 1. Example:
-
-    background, mylayer * 0.5
-    """
+    usage = "Usage: %prog [options] svgfilename"
     parser = OptionParser(usage=usage)
-    parser.add_option("-i", "--imageexport",
-            action="store_true", dest="imageexport", default=False,
-            help="Use PNG files as export content")
+    parser.add_option("-i", "--imageexport", action="store_true", dest="imageexport", default=False, help="Use PNG files as export content")
     (options, args) = parser.parse_args()
-    try:
-        FILENAME = args[0]
-    except IndexError:
-        parser.print_help()
-        sys.exit(1)
 
-    # Load the file
+    FILENAME = args[0]
+
+
+    # Take the Wireframes.svg
     f = open(FILENAME)
     cnt = f.read()
     f.close()
@@ -67,28 +46,34 @@ layer name. The opacity must be between 0 and 1. Example:
     doc = lxml.etree.fromstring(cnt)
 
     # Get all layers
-    ink_groupmode = '{http://www.inkscape.org/namespaces/inkscape}groupmode'
-    w3c_svg_tag = '{http://www.w3.org/2000/svg}g'
-    layers = [layer for layer in doc.iterdescendants(tag=w3c_svg_tag)
-            if layer.attrib.get(ink_groupmode, False) == 'layer']
-
-    # inkscape names for certain things in the svg
-    ink_label = '{http://www.inkscape.org/namespaces/inkscape}label'
+    layers = [x for x in doc.iterdescendants(tag='{http://www.w3.org/2000/svg}g') if x.attrib.get('{http://www.inkscape.org/namespaces/inkscape}groupmode', False) == 'layer']
 
     # Scan the 'content' layer
-    content_layer = [layer for layer in layers
-            if layer.attrib.get(ink_label, False).lower() == 'content']
+    content_layer = [x for x in layers if x.attrib.get('{http://www.inkscape.org/namespaces/inkscape}label', False).lower() == 'content']
 
     if not content_layer:
-        print "No 'content'-labeled layer found. See --help for more"
+        print "No 'content'-labeled layer. Create a 'content'-labeled layer and "\
+              "put a text box (no flowRect), with each line looking like:"
+        print ""
+        print "   background, layer1"
+        print "   background, layer2"
+        print "   background, layer2, layer3"
+        print "   background, layer2 * 0.5, layer3"
+        print "   +layer4 * 0.5"
+        print ""
+        print "each name being the label of another layer. Lines starting with"
+        print "a '+' will add to the layers of the preceding line, creating"
+        print "incremental display (note there must be no whitespace before '+')"
+        print ""
+        print "The opacity of a layer can be set to 50% for example by adding "
+        print "'*0.5' after the layer name."
         sys.exit(1)
 
     content = content_layer[0]
 
     # Find the text stuff, everything starting with SLIDE:
     #   take all the layer names separated by ','..
-    ink_tspan = '{http://www.w3.org/2000/svg}text/{http://www.w3.org/2000/svg}tspan'
-    preslides = [x.text for x in content.findall(ink_tspan) if x.text]
+    preslides = [x.text for x in content.findall('{http://www.w3.org/2000/svg}text/{http://www.w3.org/2000/svg}tspan') if x.text]
 
 
     if not bool(preslides):
@@ -97,29 +82,29 @@ layer name. The opacity must be between 0 and 1. Example:
         sys.exit(1)
 
 
+    #print preslides
 
 
     # Get the initial style attribute and keep it
     orig_style = {}
-    for layer in layers:
-        label = layer.attrib.get(ink_label)
-        if 'style' not in layer.attrib:
-            layer.set('style', '')
+    for l in layers:
+        label = l.attrib.get('{http://www.inkscape.org/namespaces/inkscape}label')
+        if 'style' not in l.attrib:
+            l.set('style', '')
         # Save initial values
-        orig_style[label] = layer.attrib['style']
+        orig_style[label] = l.attrib['style']
 
 
-    # slides contains seq of [('layer', opacity), ('layer', opacity), ..]
-    slides = []
-    for slide in preslides:
-        if slide:
-            if slide.startswith('+'):
-                slide = slide[1:]
+    slides = [] # Contains seq of [('layer', opacity), ('layer', opacity), ..]
+    for sl in preslides:
+        if sl:
+            if sl.startswith('+'):
+                sl = sl[1:]
                 sl_layers = slides[-1].copy()
             else:
                 sl_layers = {}
 
-            for layer in slide.split(','):
+            for layer in sl.split(','):
                 elements = layer.strip().split('*')
                 name = elements[0].strip()
                 opacity = None
@@ -130,23 +115,20 @@ layer name. The opacity must be between 0 and 1. Example:
 
 
     def set_style(el, style, value):
-        """
-        Set the display: style, add it if it isn't there, don't touch the
-        rest.
+        """Set the display: style, add it if it isn't there, don't touch the
+        rest
         """
         if re.search(r'%s: ?[a-zA-Z0-9.]*' % style, el.attrib['style']):
-            el.attrib['style'] = re.sub(
-                    r'(.*%s: ?)([a-zA-Z0-9.]*)(.*)' % style,
-                    r'\1%s\3' % value, el.attrib['style'])
+            el.attrib['style'] = re.sub(r'(.*%s: ?)([a-zA-Z0-9.]*)(.*)' % style,
+                                        r'\1%s\3' % value, el.attrib['style'])
         else:
-            el.attrib['style'] = '%s:%s;%s' % \
-                    (style, value, el.attrib['style'])
+            el.attrib['style'] = '%s:%s;%s' % (style, value, el.attrib['style'])
 
 
     pdfslides = []
     for i, slide_layers in enumerate(slides):
         for l in layers:
-            label = l.attrib.get(ink_label)
+            label = l.attrib.get('{http://www.inkscape.org/namespaces/inkscape}label')
             # Set display mode to original
             l.set('style', orig_style[label])
 
@@ -166,25 +148,24 @@ layer name. The opacity must be between 0 and 1. Example:
         # Use the correct extension if using images
         if options.imageexport:
             pdfslide = os.path.abspath(os.path.join(os.curdir,
-                    ".inkscapeslide_%s.p%05d.png" % (FILENAME, i)))
+                                                ".inkscapeslide_%s.p%05d.png" % (FILENAME, i)))
 
         # Write the XML to file, "wireframes.p1.svg"
         f = open(svgslide, 'w')
         f.write(lxml.etree.tostring(doc))
         f.close()
 
-        # Determine whether to export pdf's or images (e.g. inkscape -A versus
-        # inkscape -e)
-        cmd = "inkscape -A %s %s" % (pdfslide, svgslide)
+        # Determine whether to export pdf's or images (e.g. inkscape -A versus inkscape -e)
+        cmd = "inkscape -D -A %s %s" % (pdfslide, svgslide)
         if options.imageexport:
-            cmd = "inkscape -d 180 -e %s %s" % (pdfslide, svgslide)
+            cmd = "inkscape -D -d 180 -e %s %s" % (pdfslide, svgslide)
 
         # Using subprocess to hide stdout
         subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).communicate()
         os.unlink(svgslide)
         pdfslides.append(pdfslide)
 
-        print "Generated page %d." % (i + 1)
+        print "Generated page %d." % (i+1)
 
     joinedpdf = False
     outputFilename = "%s.pdf" % FILENAME.split(".svg")[0]
@@ -196,11 +177,10 @@ layer name. The opacity must be between 0 and 1. Example:
         if not os.system('which convert > /dev/null'):
             print "Using 'convert' to join PNG's"
             pngPath = os.path.join(outputDir, ".inkscapeslide_*.png")
-            proc = subprocess.Popen(
-                    "convert %s -resample 180 %s" % (pngPath, outputFilename),
-                    shell=True,
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.PIPE)
+            proc = subprocess.Popen("convert %s -resample 180 %s" % (pngPath, outputFilename),
+                                    shell=True,
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE)
             # See if the command succeeded
             stdout_value, stderr_value = proc.communicate()
             if proc.returncode:
@@ -239,8 +219,8 @@ layer name. The opacity must be between 0 and 1. Example:
         elif not os.system('which pdfjoin > /dev/null'):
             # In the end, run: pdfjoin wireframes.p*.pdf -o Wireframes.pdf
             print "Using 'pdfsam' to join PDFs"
-            os.system("pdfjoin --outfile %s.pdf %s" %
-                    (FILENAME.split(".svg")[0], " ".join(pdfslides)))
+            os.system("pdfjoin --outfile %s.pdf %s" % (FILENAME.split(".svg")[0],
+                                                       " ".join(pdfslides)))
             joinedpdf = True
 
         # Verify pdftk exists in PATH
@@ -248,11 +228,11 @@ layer name. The opacity must be between 0 and 1. Example:
             # run: pdftk in1.pdf in2.pdf cat output Wireframes.pdf
             print "Using 'pdftk' to join PDFs"
             os.system("pdftk %s cat output %s.pdf" % (" ".join(pdfslides),
-                    FILENAME.split(".svg")[0]))
+                                                       FILENAME.split(".svg")[0]))
             joinedpdf = True
         else:
-            print "Please install pdfjam, pdftk or install the 'pyPdf'" \
-                    "python package, to join PDFs."
+            print "Please install pdfjam, pdftk or install the 'pyPdf' python " \
+                "package, to join PDFs."
 
     # Clean up
     if joinedpdf:
